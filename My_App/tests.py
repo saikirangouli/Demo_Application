67ca83e70177e74a580bff7404c0766c50402c25
saikirangouli <saikiranreddy99124@gmail.com>
2023-09-05 23:07:44 +0530

    Test cases removed

--- My_App/tests.py
+++ My_App/tests.py
@@ -19,17 +19,7 @@ class ItemAPITest(TestCase):
         user_exists = User.objects.filter(name=self.user_name).first()
         self.assertEqual(user_exists.name, self.user_name)
 
-    # def test_update_user(self):
-    #     update_url = f'{self.endpoint}{self.user_id}/'
-    #     response = self.client.put(update_url,{'name':'Kiran','age':30}, format='json')
-    #     self.assertEqual(response.status_code,status.HTTP_200_OK)
 
-    # def test_delete_object(self):
-    #     delete_url =  f'{self.endpoint}{self.user_id}/'
-    #     print(delete_url)
-    #     response = self.client.delete(delete_url)
-    #     self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-    #     self.assertFalse(YourModel.objects.filter(pk=self.model_instance.pk).exists())
 
        
 
