980dc1ffc8adcb183a9769e70027b0d25c4d7ff2
saikirangouli <92015246+saikirangouli@users.noreply.github.com>
2023-10-11 16:57:22 +0530

    added tests

--- My_App/tests.py
+++ My_App/tests.py
@@ -6,11 +6,18 @@ class ItemAPITest(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.endpoint = '/app/users/'
+        self.user_data = {'name':'normal user','age':25}
+        self.normal_user_object = User.objects.create(**self.user_data)
         self.user_name = 'test user'
         self.age = 26
         self.user_id = 1
+        self.updated_user_name = 'Updated test user'
+        self.updated_age=23
     def test_get_all_users(self):
         response = self.client.get(f'{self.endpoint}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_get_specific_user(self):
+        response = self.client.get(f'{self.endpoint}{self.normal_user_object.pk}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_user(self):
@@ -19,9 +26,28 @@ class ItemAPITest(TestCase):
         user_exists = User.objects.filter(name=self.user_name).first()
         self.assertEqual(user_exists.name, self.user_name)
 
+    def test_update_user(self):
+        print(f'{self.endpoint}{self.normal_user_object.pk}')
+        response = self.client.put(f'{self.endpoint}{self.normal_user_object.pk}/',{'name': self.updated_user_name,'age':self.updated_age}, format='json')
+        print(response.status_code)
+        self.assertTrue(User.objects.filter(name=self.updated_user_name).exists())
+        
+    def test_delete_user(self):
+        self.assertTrue(User.objects.filter(name=self.user_data['name']).exists())
+        response = self.client.delete(f'{self.endpoint}',{'name': self.user_name}, format='json')
+        self.assertFalse(User.objects.filter(name=self.user_name).exists())
+
+    
+
 
 
        
+
+
+
+
+
+
 
 
 
